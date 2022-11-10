99ff8cad9680402486a2cc57a21e3d74ddb67c4e
Isha <72744901+IshaMeera@users.noreply.github.com>
2022-11-10 10:45:13 +0530

    Update model.py

--- Assignment/Isha-Team Member/Assignment 1/model.py
+++ Assignment/Isha-Team Member/Assignment 1/model.py
@@ -1,1 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thur Nov 10 10:45:40 2022
+@author: Isha
+"""
 
+from flask_login import UserMixin
+from __init__ import db
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(1000))
